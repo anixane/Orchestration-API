e35cb1355d96ae1f0457443ea348014217503aff
Animesh Kumar <pausethismoment@gmail.com>
2020-08-02 10:59:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,126 +22,115 @@ def print_item(group):
     if hasattr(group, 'properties'):
         print_properties(group.properties)
 
-
 def print_properties(props):
     """Print a ResourceGroup properties instance."""
     if props and hasattr(props, 'provisioning_state') and props.provisioning_state:
         print("\tProperties:")
         print("\t\tProvisioning State: {}".format(props.provisioning_state))
     print("\n\n")
+    
+def CreateADF(rg_name,df_name,df_params,adf_client):
+  adf_identity = FactoryIdentity(type='SystemAssigned',principal_id=CLIENT_ID,tenant_id=TENANT_ID)
+  df_resource = Factory(location='eastus',identity=adf_identity)
+  df = adf_client.factories.create_or_update(rg_name, df_name, df_resource)
+  print_item(df)
+  while df.provisioning_state != 'Succeeded':
+    df = adf_client.factories.get(rg_name, df_name)
+    time.sleep(1)
+  return df, adf_client
 
+def CreateAzureKeyVaultLinkedService(rg_name, df_name, AKV_LS_NAME,keyvaultName,adf_client):
+  ls_azure_storage = AzureKeyVaultLinkedService(type='str',base_url='https://' + keyvaultName +'.vault.azure.net/')
+  ls = adf_client.linked_services.create_or_update(rg_name, df_name, AKV_LS_NAME, ls_azure_storage)
+  print_item(ls)
+  return ls, adf_client
 
-def CreateADF(rg_name, df_name, df_params, adf_client):
-    adf_identity = FactoryIdentity(
-        type='SystemAssigned', principal_id=CLIENT_ID, tenant_id=TENANT_ID)
-    df_resource = Factory(location='eastus', identity=adf_identity)
-    df = adf_client.factories.create_or_update(rg_name, df_name, df_resource)
-    while df.provisioning_state != 'Succeeded':
-        df = adf_client.factories.get(rg_name, df_name)
-        time.sleep(1)
-    return df, adf_client
+def CreateAzureDataLakeStorageLinkedService(rg_name, df_name, ls_name,ADLSURI,adf_client,AKVsecret_name,AzureKVreference_name):
+  lsr=LinkedServiceReference(type = 'LinkedServiceReference',reference_name= AzureKVreference_name)
+  account_key= AzureKeyVaultSecretReference(type ='str',store=lsr, secret_name=AKVsecret_name)
+  ls_azure_storage = AzureDataLakeStoreLinkedService(data_lake_store_uri=ADLSURI ,service_principal_id=CLIENT_ID,service_principal_key=account_key, tenant=TENANT_ID)
+  ls = adf_client.linked_services.create_or_update(rg_name, df_name, ls_name, ls_azure_storage)
+  print_item(ls)
+  return ls, adf_client
 
+def CreateParquetDataset(rg_name, df_name, ds_name,DATASET_FLODER_NAME,FOLDER_PATH,FILE_NAME,adf_client,ls_name,compression_TYPE='none'):
+  ds_ls = LinkedServiceReference(reference_name=ls_name)
+  DatasetLocation_adls=AzureDataLakeStoreLocation(folder_path=FOLDER_PATH,file_name=FILE_NAME)
+  folder_dataset = DatasetFolder(name=DATASET_FLODER_NAME)
+  ds_azure_adls = ParquetDataset(
+      linked_service_name=ds_ls, folder=folder_dataset,type='str',location=DatasetLocation_adls,compression_codec=compression_TYPE)
+  ds = adf_client.datasets.create_or_update(
+      rg_name, df_name, ds_name, ds_azure_adls)
+  print_item(ds)
+  return ds, adf_client
 
-def CreateAzureKeyVaultLinkedService(rg_name, df_name, AKV_LS_NAME, keyvaultName, adf_client):
-    ls_azure_storage = AzureKeyVaultLinkedService(
-        type='str', base_url='https://' + keyvaultName + '.vault.azure.net/')
-    ls = adf_client.linked_services.create_or_update(
-        rg_name, df_name, AKV_LS_NAME, ls_azure_storage)
-    print_item(ls)
-    return ls, adf_client
+def CreateCopyActivity(Activity_Name,Sourceadls, Sinkadls):
+  ADLS_source = ParquetSource()
+  ADLS_sink = ParquetSink()
+  Sourcein_ref = DatasetReference(reference_name=Sourceadls)
+  SinkOut_ref = DatasetReference(reference_name=Sinkadls)
+  copy_activity = CopyActivity(name=Activity_Name, inputs=[Sourcein_ref], outputs=[SinkOut_ref], source=ADLS_source, sink=ADLS_sink)
+  return copy_activity
 
-
-def CreateAzureDataLakeStorageLinkedService(rg_name, df_name, ls_name, ADLSURI, adf_client, AKVsecret_name, AzureKVreference_name):
-    lsr = LinkedServiceReference(
-        type='LinkedServiceReference', reference_name=AzureKVreference_name)
-    account_key = AzureKeyVaultSecretReference(
-        type='str', store=lsr, secret_name=AKVsecret_name)
-    ls_azure_storage = AzureDataLakeStoreLinkedService(
-        data_lake_store_uri=ADLSURI, service_principal_id=CLIENT_ID, service_principal_key=account_key, tenant=TENANT_ID)
-    ls = adf_client.linked_services.create_or_update(
-        rg_name, df_name, ls_name, ls_azure_storage)
-    print_item(ls)
-    return ls, adf_client
-
-
-def CreateParquetDataset(rg_name, df_name, ds_name, DATASET_FLODER_NAME, FOLDER_PATH, FILE_NAME, adf_client, ls_name, compression_TYPE='none'):
-    ds_ls = LinkedServiceReference(reference_name=ls_name)
-    DatasetLocation_adls = AzureDataLakeStoreLocation(
-        folder_path=FOLDER_PATH, file_name=FILE_NAME)
-    folder_dataset = DatasetFolder(name=DATASET_FLODER_NAME)
-    ds_azure_adls = ParquetDataset(
-        linked_service_name=ds_ls, folder=folder_dataset, type='str', location=DatasetLocation_adls, compression_codec=compression_TYPE)
-    ds = adf_client.datasets.create_or_update(
-        rg_name, df_name, ds_name, ds_azure_adls)
-    print_item(ds)
-    return ds, adf_client
-
-
-def CreateCopyActivity(Activity_Name, Sourceadls, Sinkadls):
-    ADLS_source = ParquetSource()
-    ADLS_sink = ParquetSink()
-    Sourcein_ref = DatasetReference(reference_name=Sourceadls)
-    SinkOut_ref = DatasetReference(reference_name=Sinkadls)
-    copy_activity = CopyActivity(name=Activity_Name, inputs=[Sourcein_ref], outputs=[
-                                 SinkOut_ref], source=ADLS_source, sink=ADLS_sink)
-    return copy_activity
-
-
-def CreatePipeline(adf_client, rg_name, df_name, p_name, Activity=[]):
-    params_for_pipeline = {}
-    p_obj = PipelineResource(
-        activities=Activity, parameters=params_for_pipeline)
-    print(p_obj)
-    p = adf_client.pipelines.create_or_update(rg_name, df_name, p_name, p_obj)
-    return p, adf_client
-
+def CreatePipeline(adf_client,rg_name,df_name,p_name,Activity):
+  params_for_pipeline = {}
+  print('start creating pipeline object')
+  p_obj = PipelineResource(activities=Activity, parameters=params_for_pipeline)
+  print('start creating pipeline with copy activity')
+  print(p_obj)
+  p = adf_client.pipelines.create_or_update(rg_name, df_name, p_name, p_obj)
+  return p, adf_client
 
 def DeleteADF(adf_client, rg_name, df_name):
     adf_client.factories.delete(rg_name, df_name)
 
 
-def DynamicADF(rg_name, df_name, subscription_id, TENANT_ID, CLIENT_ID, keyvaultName, AKVsecret_name, ADLSURI, SRC_FOLDER_PATH, SINK_FOLDER_PATH, FILE_NAME):
-    credentials = ServicePrincipalCredentials(client_id=CLIENT_ID,
+def DynamicADF(rg_name,df_name,subscription_id,TENANT_ID,CLIENT_ID,keyvaultName,AKVsecret_name,Source_ADLSURI,Sink_ADLSURI,SRC_FOLDER_PATH,SINK_FOLDER_PATH,FILES_NAME=[]):
+  credentials = ServicePrincipalCredentials(client_id=CLIENT_ID,
                                               secret='..6AmP?Osx2M4/UU40Hd[0LE]Y@BxqdS', tenant=TENANT_ID)
-    adf_client = DataFactoryManagementClient(credentials, subscription_id)
+  adf_client = DataFactoryManagementClient(credentials, subscription_id)
 
-    rg_params = {'location': 'eastus'}
-    df_params = {'location': 'eastus'}
+  rg_params = {'location':'eastus'}
+  df_params = {'location':'eastus'}
 
-    adf, adf_client = CreateADF(rg_name, df_name, df_params, adf_client)
+  adf,adf_client = CreateADF(rg_name, df_name, df_params, adf_client)
 
-    AzureKeyVaultLinkedService, adf_client = CreateAzureKeyVaultLinkedService(
-        rg_name, df_name, 'AzureKeyVaultLinkedService', keyvaultName, adf_client)
+  AzureKeyVaultLinkedService, adf_client =CreateAzureKeyVaultLinkedService(rg_name, df_name, 'AzureKeyVaultLinkedService',keyvaultName,adf_client)
 
-    AzureDataLakeStorageLinkedService, adf_client = CreateAzureDataLakeStorageLinkedService(
-        rg_name, df_name, 'AzureDataLakeStorageLinkedService', ADLSURI, adf_client, AKVsecret_name, 'AzureKeyVaultLinkedService')
-
-    SourceprqDataset, adf_client = CreateParquetDataset(rg_name, df_name, 'SourceprqDataset', 'staging',
-                                                        SRC_FOLDER_PATH, FILE_NAME, adf_client, 'AzureDataLakeStorageLinkedService', compression_TYPE='none')
-
-    SinkprqDataset, adf_client = CreateParquetDataset(
-        rg_name, df_name, 'SinkprqDataset', 'staging', SINK_FOLDER_PATH, FILE_NAME, adf_client, 'AzureDataLakeStorageLinkedService', compression_TYPE='none')
-    copy_activity = CreateCopyActivity(
-        'COPYparquetfile', 'SourceprqDataset', 'SinkprqDataset')
-
-    pipeline, adf_client = CreatePipeline(
-        adf_client, rg_name, df_name, 'CopyAdlsToAdls', [copy_activity])
-    return adf_client
+  AzureDataLakeStorageLinkedService, adf_client = CreateAzureDataLakeStorageLinkedService(rg_name,df_name,'AzureDataLakeStorageLinkedService',Source_ADLSURI,adf_client,AKVsecret_name,'AzureKeyVaultLinkedService')
+  
+  print("Fille list are\n",FILES_NAME)
+  
+  avtivity=[]
+  for FILE_NAME in FILES_NAME:
+    print("crate datasets for\n",FILES_NAME)
+    
+    SourceprqDataset, adf_client=CreateParquetDataset(rg_name,df_name,FILE_NAME+'SourceprqDataset','staging',SRC_FOLDER_PATH,FILE_NAME,adf_client,'AzureDataLakeStorageLinkedService',compression_TYPE='none')
+  
+    SinkprqDataset, adf_client=CreateParquetDataset(rg_name,df_name,FILE_NAME+'SinkprqDataset','staging',SINK_FOLDER_PATH,FILE_NAME,adf_client,'AzureDataLakeStorageLinkedService',compression_TYPE='none')                 
+    FILE_NAME = CreateCopyActivity(FILE_NAME,FILE_NAME+'SourceprqDataset',FILE_NAME+'SinkprqDataset')
+    avtivity.append(FILE_NAME)
+    print(avtivity)
+  pipeline, adf_client = CreatePipeline(adf_client,rg_name,df_name,'CopyAdlsToAdls',avtivity)
+    
+  return adf_client
 
 
 app = Flask(__name__)
 # defining global variables
 rg_name = 'OrchestrationTestResourceGroup'
-df_name = 'testadfnew08'
-subscription_id = '5fd4b8a3-3bd7-4592-89ba-6a56ace0a00b'
+df_name = 'testadfnew02'
+subscription_id = '8706626e-72cb-4bc5-9e7c-ebfe52d0c2ba'
 TENANT_ID = '7d46cab4-943a-4332-8961-e7fbce0633cf'
 CLIENT_ID = '88ad9879-d7d1-4ce0-b3b9-e1248ed6ebc9'
 keyvaultName = 'orchestrationkeyvault'
 AKVsecret_name = 'client-secret'
 ADLSURI = 'adl://stagingadls.azuredatalakestore.net'
+Source_ADLSURI='adl://stagingadls.azuredatalakestore.net'
+Sink_ADLSURI='adl://stagingadls.azuredatalakestore.net'
 SRC_FOLDER_PATH = ''
 SINK_FOLDER_PATH = 'Output'
-FILE_NAME = 'DimAccount'
+FILES_NAME= ["Test1","Test"]
 SourceSTORAGE_TYPE = 'ADLS'
 
 
@@ -188,8 +177,7 @@ def stagingUpstreamForm():
         SRC_FOLDER_PATH = response['stagingUpstreamADLSFolderPath']
         FILE_NAME = response['stagingUpstreamADLSFileName']
 
-        DynamicADF(rg_name, df_name, subscription_id, TENANT_ID, CLIENT_ID, keyvaultName,
-                   AKVsecret_name, ADLSURI, SRC_FOLDER_PATH, SINK_FOLDER_PATH, FILE_NAME)
+        DynamicADF(rg_name,df_name,subscription_id,TENANT_ID,CLIENT_ID,keyvaultName,AKVsecret_name,Source_ADLSURI,Sink_ADLSURI,SRC_FOLDER_PATH,SINK_FOLDER_PATH,FILES_NAME)
 
         return render_template('successfulDeploy.html', DataFactory_Name=str(df_name))
     return render_template('stagingUpstreamForm.html')
